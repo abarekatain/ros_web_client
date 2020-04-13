17157231ede0f6385f03ec685eecaf38ca9e2416
abarekatain <alireza.barekatain1996@gmail.com>
2020-04-13 07:55:43 -0400

    feat: Finish ConfigParser

--- src/ros_web_client/initialization.py
+++ src/ros_web_client/initialization.py
@@ -1,6 +1,10 @@
 import json
 
-from ros_web_client.message_wrapper import Topic
+from ros_web_client.message_wrapper import Topic, Param
+
+from rosbridge_library.rosbridge_protocol import RosbridgeProtocol
+
+
 class ConfigParser(object):
     """ Parser for Initialization config file in JSON
 
@@ -11,10 +15,42 @@ class ConfigParser(object):
     def __init__(self, config):
         self.config = json.loads(config)
         self.list_topics = []
+        self.list_params = []
+        self.list_commands = []
     
     def parse_topics(self):
         for item in self.config.get("topics"):
             topic=Topic(item["name"],item["message_type"],parameters=item)
             self.list_topics.append(topic)
+            self.list_commands.append(topic.subscribe_command())
+    
+    def parse_params(self):
+        for item in self.config.get("parameters"):
+            param= Param(item)
+            self.list_params.append(param)
+            self.list_commands.append(param.get_command())
+    
+    def parse(self):
+        self.parse_topics()
+        self.parse_params()
+        return self.list_commands
+
+class CommandExecuter(object):
+
+    def __init__(self):
+        self.protocol = RosbridgeProtocol(0)
+        self.protocol.outgoing = self.outgoing
+        self.list_responses = {}
+
+    def outgoing(self, message):
+        pass    
+    
+    def execute(self, list_commands):
+        for command in list_commands:
+            self.protocol.incoming(command)
+
+
+
+
 
             
