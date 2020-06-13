f1f1bd5385197e8dee2618e84b3410df48e5ebc6
abarekatain <alireza.barekatain1996@gmail.com>
2020-06-13 09:36:46 -0400

    test: Sync Topics

--- test/sync_topic_tests/sync_test.py
+++ test/sync_topic_tests/sync_test.py
@@ -9,7 +9,19 @@ from tf2_msgs.msg import TFMessage
 
 
 class Test(unittest.TestCase):
-    pass
+    
+    def test_sync(self):
+        with open('compressedDepth_sample_msg.txt', 'r') as outfile:
+            sample_depth = outfile.read()  
+
+        with open('compressedRGB_sample_msg.txt', 'r') as outfile:
+            sample_rgb = outfile.read()  
+
+        from rosbridge_library.rosbridge_protocol import RosbridgeProtocol
+        test_protocol=RosbridgeProtocol(88)
+
+        test_protocol.incoming(sample_depth)
+        test_protocol.incoming(sample_rgb)
 
 
 
@@ -26,8 +38,10 @@ if __name__ == '__main__':
 
 
 
+
     def callback_rgb(data):
         print("=============RGB Header==============")
+        print(rospy.get_rostime().secs)
         print(data.header)
 
     def callback_tf(data):
@@ -36,14 +50,19 @@ if __name__ == '__main__':
 
 
 
-    rospy.Subscriber("/webclient/myimage/image_raw", Image, callback_depth)
-    rospy.Subscriber("/webclient/camera/rgb/image_raw", Image, callback_rgb)
+    rospy.Subscriber("/myimage/compressedDepth", CompressedImage, callback_depth)
+    rospy.Subscriber("/camera/rgb/image_raw/compressed", CompressedImage, callback_rgb)
     #rospy.Subscriber("/tf", TFMessage, callback_tf)
+
+    test_case.test_sync()
+
+    
 
 
 
     
     
     while not rospy.is_shutdown():
+        print(rospy.Time.now().secs)
         rospy.spin()
     
